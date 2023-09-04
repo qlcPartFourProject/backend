6f25f9f921ec2177704f9482ff391bdb3c318391
qlcPartFourProject <qlc.part4project@gmail.com>
2023-09-05 11:54:45 +1200

    chore: removed unused files

--- answers/Answer.py
+++
@@ -1,7 +0,0 @@
-class Answer:
-    def __init__(self, text, isCorrect):
-        self.text = text
-        self.isCorrect = isCorrect
-
-    def __str__(self):
-        return self.text
--- questions/QuestionGenerator.py
+++
@@ -1,8 +0,0 @@
-from questions.Question import Question
-
-class QuestionGenerator():
-    def __init__(self, stats):
-        self.stats = stats
-
-    def generate_question(self, question_type: Question):
-        return question_type(self.stats)
